18c7271a546400d6a9b8c4975187380fd5728a6c
Jiří Orság <georg77@gmail.com>
2015-07-01 16:02:22 +0200

    Add favicons

---
+++ src/server/frontend/favicons.js
@@ -0,0 +1,141 @@
+export default [
+  {
+    rel:   'apple-touch-icon',
+    sizes: '57x57',
+    href:  'apple-touch-icon-57x57.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '60x60',
+    href:  'apple-touch-icon-60x60.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '72x72',
+    href:  'apple-touch-icon-72x72.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '76x76',
+    href:  'apple-touch-icon-76x76.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '114x114',
+    href:  'apple-touch-icon-114x114.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '120x120',
+    href:  'apple-touch-icon-120x120.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '144x144',
+    href:  'apple-touch-icon-144x144.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '152x152',
+    href:  'apple-touch-icon-152x152.png'
+  },
+  {
+    rel:   'apple-touch-icon',
+    sizes: '180x180',
+    href:  'apple-touch-icon-180x180.png'
+  },
+  {
+    rel:   'icon',
+    type:  'image/png',
+    href:  'favicon-32x32.png',
+    sizes: '32x32'
+  },
+  {
+    rel:   'icon',
+    type:  'image/png',
+    href:  'favicon-194x194.png',
+    sizes: '194x194'
+  },
+  {
+    rel:   'icon',
+    type:  'image/png',
+    href:  'favicon-96x96.png',
+    sizes: '96x96'
+  },
+  {
+    rel:   'icon',
+    type:  'image/png',
+    href:  'android-chrome-192x192.png',
+    sizes: '192x192'
+  },
+  {
+    rel:   'icon',
+    type:  'image/png',
+    href:  'favicon-16x16.png',
+    sizes: '16x16'
+  },
+  {
+    rel:  'manifest',
+    href: 'manifest.json'
+  },
+  // iPad retina portrait startup image
+  {
+    href:  'apple-touch-startup-image-1536x2008.png',
+    media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+    rel:   'apple-touch-startup-image'
+  },
+  // iPad retina landscape startup image
+  {
+    href:  'apple-touch-startup-image-1496x2048.png',
+    media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)',
+    rel:   'apple-touch-startup-image'
+  },
+  // iPad non-retina portrait startup image
+  {
+    href:  'apple-touch-startup-image-768x1004.png',
+    media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: portrait)',
+    rel:   'apple-touch-startup-image'
+  },
+  // iPad non-retina landscape startup image
+  {
+    href:  'apple-touch-startup-image-748x1024.png',
+    media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 1) and (orientation: landscape)',
+    rel:   'apple-touch-startup-image'
+  },
+  // iPhone 6+ portrait startup image
+  {
+    href:  'apple-touch-startup-image-1242x2148.png',
+    media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+    rel:   'apple-touch-startup-image'
+  },
+  // iPhone 6+ landscape startup image
+  {
+    href:  'apple-touch-startup-image-1182x2208.png',
+    media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)',
+    rel:   'apple-touch-startup-image'
+  },
+  // iPhone 6 startup image
+  {
+    href:  'apple-touch-startup-image-750x1294.png',
+    media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+    rel:   'apple-touch-startup-image'
+  },
+  // Phone 5 startup image
+  {
+    href:  'apple-touch-startup-image-640x1096.png',
+    media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+    rel:   'apple-touch-startup-image'
+  },
+  // Phone < 5 retina startup image
+  {
+    href:  'apple-touch-startup-image-640x920.png',
+    media: '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 2)',
+    rel:   'apple-touch-startup-image'
+  },
+  // Phone < 5 non-retina startup image
+  {
+    href:  'apple-touch-startup-image-320x460.png',
+    media: '(device-width: 320px) and (device-height: 480px) and (-webkit-device-pixel-ratio: 1)',
+    rel:   'apple-touch-startup-image'
+  }
+]
