7e3e4eff82cf643890104eb632406aa63ad6db11
David Sljukic <davidsljukic@gmail.com>
2015-08-15 23:33:01 +0200

    Change exclamination with a dot

--- src/client/pages/home.react.js
+++ src/client/pages/home.react.js
@@ -8,7 +8,7 @@ class Home extends Component {
       <div className='home-page'>
         <h1 className='greetings-header'>Hello!</h1>
         <p className='greetings-paragraph'>
-          I'm David. I'm an software engineer<br/>and I'm looking forward to working with you!
+          I'm David. I'm an software engineer<br/>and I'm looking forward to working with you.
         </p>
       </div>
     );
