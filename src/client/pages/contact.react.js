8e1bfe9511e99bf728140a9066ba4aea32d8752e
David Sljukic <davidsljukic@gmail.com>
2015-08-16 22:31:11 +0200

    Fixup contact page

--- src/client/pages/contact.react.js
+++ src/client/pages/contact.react.js
@@ -5,8 +5,11 @@ export default class Contact extends Component {
 
   render() {
     return (
-      <div>     
-        <span>CONTACT</span>
+      <div className='contact-page'>
+        <p className='signature mediterano'>David Šljukić</p>
+        <p>email: <a className='mediterano' href='mailto:david.sljukic@gmail.com' target='_blank'>davidsljukic@gmail.com</a></p>
+        <p>github: <a className='mediterano' href='https://github.com/sljuka' target='_blank'>https://github.com/sljuka</a></p>
+        <p>skype: <span className='mediterano'>david.sljukic</span></p>
       </div>
     )
   }
